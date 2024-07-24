a1398e559b1b7f8f3c082a9dd7fd4964f7e39f5e
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-07-24 23:29:16 +0300

    chore: remove files/packages that aren't part of Etherscan client

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { type Page } from "~/util";
-
-const Home: Page = () => {
-    return <main>Hello World</main>;
-};
-
-export default Home;
--- src/util.ts
+++
@@ -1,5 +0,0 @@
-import type { FC, ReactNode } from "react";
-
-export type Layout = FC<{ children: ReactNode }>;
-
-export type Page<T extends Record<string, string> = never> = FC<{ params: T }>;
